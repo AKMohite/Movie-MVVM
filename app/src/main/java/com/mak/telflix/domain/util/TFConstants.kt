512c7ee505a9b01ce741c4e3f8f4b46a5a8ffb36
Ashish Mohite <ashishmohite1996@gmail.com>
2022-04-09 13:02:30 +0530

    added api service, dtos for popular tv

---
+++ app/src/main/java/com/mak/telflix/domain/util/TFConstants.kt
@@ -0,0 +1,7 @@
+package com.mak.telflix.domain.util
+
+object TFConstants {
+    const val API_GET_POPULAR_TV = "tv/popular"
+    const val QUERY_LANGUAGE = "language"
+    const val QUERY_PAGE = "page"
+}
