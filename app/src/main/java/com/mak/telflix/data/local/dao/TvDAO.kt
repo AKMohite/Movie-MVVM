9e1f8fa9d77becad17469cff1189dabcab51c292
Ashish Mohite <ashishmohite1996@gmail.com>
2022-04-17 12:00:08 +0530

    added delete and update page entries

--- app/src/main/java/com/mak/telflix/data/local/dao/TvDAO.kt
+++ app/src/main/java/com/mak/telflix/data/local/dao/TvDAO.kt
@@ -2,6 +2,7 @@ package com.mak.telflix.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import com.mak.telflix.data.local.entity.TvEntity
 import com.mak.telflix.domain.util.TFConstants.C_PAGE_NO
 import com.mak.telflix.domain.util.TFConstants.TABLE_TV
@@ -12,5 +13,14 @@ interface TvDAO: BaseDAO<TvEntity> {
 
     @Query("SELECT * FROM $TABLE_TV WHERE $C_PAGE_NO = :page")
     fun getTvSeries(page: Int): Flow<List<TvEntity>>
+
+    @Query("DELETE FROM $TABLE_TV WHERE $C_PAGE_NO = :page")
+    suspend fun deletePage(page: Int)
+
+    @Transaction
+    suspend fun updatePage(page: Int, entities: List<TvEntity>) {
+        deletePage(page)
+        insertAll(entities)
+    }
 
 }
